9ca09bf264ff1996cd6c9d931521b679c2072c21
Drew Botka <hellodrewbot@gmail.com>
2014-05-29 08:22:16 -0400

    Created several unit tests and a constructor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,49 @@
 
 
+var imageArray = new Array("images/img1.jpg", "images/img2.jpg", "images/img3.jpg");
+
+function Slider(array, location, interval, fade, height) {
+	var imageCount = array.length,
+	counter = 1;
+	if (!$.isArray(array)){
+		throw new Error ('Thats not an array');
+	} else if (array.length <= 0) {
+		throw new Error ('Array must have content');
+	} else {
+		$(location).attr('src', array[0]);
+		$(location).attr('height', height);
+			setInterval(function() {
+				$(location).fadeOut(fade, function() {
+				$(this).attr('src', array[counter]).fadeIn(fade);
+				counter = counter == imageCount - 1 ? 0 : counter += 1;
+				$(this).attr('height', height)
+			});
+		}, interval);
+	};
+}
+
+var nameYourSlider = new Slider(imageArray, '.slider-images', 2500, 500, '100px');
+
+//Includes height
+
+// function Slider(array, location, interval, fade, height) {
+// 	var imageCount = array.length,
+// 	counter = 1;
+// 	if (!$.isArray(array)){
+// 		throw new Error ('Thats not an array');
+// 	} else if (array.length <= 0) {
+// 		throw new Error ('Array must have content');
+// 	} else {
+// 		$(location).attr('src', array[0]);
+// 		$(location).attr('height', height)
+// 			setInterval(function() {
+// 				$(location).fadeOut(fade, function() {
+// 				$(this).attr('src', array[counter]).fadeIn(fade);
+// 				counter = counter == imageCount - 1 ? 0 : counter += 1;
+// 				$(this).attr('height', height)
+// 			});
+// 		}, interval);
+// 	};
+// }
+
+// var nameYourSlider = new Slider(imageArray, '.slider-images', 2500, 500, '100px');
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,12 +3,91 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-              var answer = 2;
-              expect(2).to.equal(answer);
-            });
-        });
+    var imageArray = new Array("images/img1.jpg", "images/img2.jpg", "images/img3.jpg");
+
+    describe("the slider constuctor", function(){
+    	
+      // it("accepts an array as an argument", function(){
+ 		
+        var sliderInstance = new Slider(imageArray);
+
+      //   expect(sliderInstance.array()).to.equal(imageArray);
+      // });
+
+      it("should show a new image based on some sort of counting mechanism", function(){
+
+      });
+
+      it("should show images in order of placement in the array", function(){
+
+      });
+
+      it("should throw an error if anything other than an array is passed through", function(){
+      	var anotherslideShow = function(){
+      		sliderInstance("string")
+      	}
+
+      	expect(anotherslideShow).to.throw(Error);
+      });
+
+      it("should throw an error if an empty array is passed through", function(){
+      	var anotherbadslideShow = function(){
+      		sliderInstance([]);
+      	}
+
+      	expect(anotherbadslideShow).to.throw(Error);
+      });
     });
+
+
+    //Start Mason's example tests
+ 
+    // describe("the Die constuctor", function(){
+    //   it("correctly sets the # of sides on instances", function(){
+ 
+    //     var dieInstance = new Die(9);
+    //     expect(dieInstance.sides).to.equal(9);
+ 
+    //     var dieInstance = new Die(92837429);
+    //     expect(dieInstance.sides).to.equal(92837429);
+    //   });
+ 
+    //   it("should throw when given non-number args", function(){
+ 
+    //     var erroneousDieCall = function(){
+    //       new Die("cool");
+    //     }
+    //     expect(erroneousDieCall).to.throw(Error);
+    //   });
+    // });
+ 
+    // describe("Die instances", function(){
+ 
+    //   it("should roll random numbers in valid range", function(){
+        
+    //     var dieInstance = new Die(20);
+ 
+    //     for(var i = 0; i < 999; i += 1) {
+    //       var result = dieInstance.roll();
+    //       expect(result).to.be.within(1,20);
+    //     }
+    //   })
+    // });
+ 
+    // describe("a Die view", function(){
+ 
+    //   it("should render a die template", function(){
+ 
+    //     var dieInstance = new Die(6);
+    //     var result = renderDie(dieInstance);
+ 
+    //     expect(result).to.have.string('6');
+    //     expect(result).to.have.string('5');
+    //     expect(result).to.have.string('4');
+    //     expect(result).to.have.string('roll-button');
+    //   })
+    // });
+
+    //End Mason's example tests
+
 })();
